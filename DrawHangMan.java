8eb1bd8438be06a7ef6fc30bf1962208fa275efb
Paolo Recchia <weegee09@gmail.com>
2016-02-08 10:27:06 -0500

    draw class + runner

--- DrawHangMan.java
+++ DrawHangMan.java
@@ -1,11 +1,47 @@
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+
 public class DrawHangMan
 {
+	BufferedImage man;
+
+	//guesses will be 0-6, with 6 being losing state
 	public String getStringDrawing(int wrongGuesses)
 	{
-	
+		String res = "";
+		if(wrongGuesses>0) System.out.print("  ,-----.\n /       \\\n(         )\n \\       /\n  `--.--'\n"); //drawhead
+		if(wrongGuesses>4) System.out.print("   ,'|`,\n  ,' | `,\n /   |   \\\n<    '    >\n"); //both arms + torso
+		else
+		if(wrongGuesses>3) System.out.print("   ,'|\n  ,' |\n /   |\n<    '\n"); //left arm + torso
+		else
+		if(wrongGuesses>2) System.out.print("     |\n     |\n     |\n     |\n"); //just torso
+		return res;
 	}
-	public Image getImage(int wrongGuesses)
+	/*
+  ,-----.
+ /       \
+(         )
+ \       /
+  `--.--'
+   ,'|`,
+  ,' | `,
+ /   |   \
+<    '    >
+	 X
+	/ \
+   /   \
+  /     \
+ /       \
+/         \
+	*/
+	public BufferedImage getImage(int wrongGuesses)
 	{
-	
+		try {
+			man = ImageIO.read(new File("man0.jpg"));
+			return man;
+		}catch (IOException e){
+			return null;
+		}
 	}
 }
--- HangmanRunner.java
+++ HangmanRunner.java
@@ -1,8 +1,44 @@
+import java.util.Scanner;
+
 public class HangmanRunner
 {
 	public static void main(String[] args)
 	{
-		//HangMan game = new game();
+		HangMan man = new HangMan();
+		String result;
+		String title;
+		String dashes;
+		String incorrect;
+		String word;
+		String drawing;
+		int incorrectGuesses;
+		boolean hasWon;
+		while(hasWon = false)
+		{
+			Refresh(title,dashes,incorrect,word,drawing,incorrectGuesses,hasWon);
+			result = Picture();
+			System.out.println(result);
+		}
+	}
+	public static void Refresh(String title, String dashes, String incorrect,String word,String drawing,int incorrectGuesses,boolean hasWon)
+	{
+		title = "~~HangMan~~";
+		dashes = man.getWordSoFar();
+		incorrect = man.getIncorrectLetters();
+		word = man.getWord();
+		drawing = man.consoleDrawing();
+		incorrectGuesses = man.getAmtIncorrectGuesses();
+		hasWon = man.win();
+	}
+	public static String Picture()
+	{
+		Scanner sc = new Scanner(System.in);
+		String result = "";
+		result += title + "\nWord: " + dashes + "\n" + drawing + "\nIncorrect Guesses: " + incorrect;
+		result += "\tNumber Incorrect: " + incorrectGuesses + "\nYour Guess: " + word;
+		word = sc.nextLine();
+
+		return result;
 
 	}
 }
