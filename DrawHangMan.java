958e8b845a703116c37d82cef0b706c590ec08f5
Paolo Recchia <weegee09@gmail.com>
2016-02-09 10:24:48 -0500

    returns image
    fixed the important ascii art

--- DrawHangMan.java
+++ DrawHangMan.java
@@ -1,21 +1,22 @@
-import java.awt.image.*;
-import javax.imageio.*;
-import java.io.*;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class DrawHangMan
 {
-	BufferedImage man;
-
 	//guesses will be 0-6, with 6 being losing state
 	public String getStringDrawing(int wrongGuesses)
 	{
 		String res = "";
-		if(wrongGuesses>0) System.out.print("  ,-----.\n /       \\\n(         )\n \\       /\n  `--.--'\n"); //drawhead
-		if(wrongGuesses>4) System.out.print("   ,'|`,\n  ,' | `,\n /   |   \\\n<    '    >\n"); //both arms + torso
-		else
-		if(wrongGuesses>3) System.out.print("   ,'|\n  ,' |\n /   |\n<    '\n"); //left arm + torso
-		else
-		if(wrongGuesses>2) System.out.print("     |\n     |\n     |\n     |\n"); //just torso
+		if(wrongGuesses>0) res += "  ,-----.\n /       \\\n(         )\n \\       /\n  `--.--'\n"; //drawhead
+		if(wrongGuesses>4)
+			res += "   ,'|`,\n  ,' | `,\n /   |   \\\n<    '    >\n"; //both arms + torso
+		else if(wrongGuesses>3)
+			res += "   ,'|\n  ,' |\n /   |\n<    '\n"; //left arm + torso
+		else if(wrongGuesses>2)
+			res += "     |\n     |\n     |\n     |\n"; //just torso
 		return res;
 	}
 	/*
@@ -28,17 +29,19 @@ public class DrawHangMan
   ,' | `,
  /   |   \
 <    '    >
-	 X
-	/ \
+     X
+    / \
    /   \
   /     \
  /       \
 /         \
 	*/
-	public BufferedImage getImage(int wrongGuesses)
+	public Image getImage(int wrongGuesses)
 	{
+		Image man = null;
+
 		try {
-			man = ImageIO.read(new File("man0.jpg"));
+			man = ImageIO.read(new File("./man" + wrongGuesses + ".jpg")); //man0.jpg to man6.jpg
 			return man;
 		}catch (IOException e){
 			return null;
