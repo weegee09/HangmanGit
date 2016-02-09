71efb43fafe738c5773104b3cf272b3cb4685d0b
Paolo Recchia <weegee09@gmail.com>
2016-02-09 10:00:22 -0500

    console runner update + image base
    runner is from bill

--- HangmanRunner.java
+++ HangmanRunner.java
@@ -1,3 +1,5 @@
+//Hangman Runner(Console)
+
 import java.util.Scanner;
 
 public class HangmanRunner
@@ -16,7 +18,7 @@ public class HangmanRunner
 		while(hasWon = false)
 		{
 			Refresh(title,dashes,incorrect,word,drawing,incorrectGuesses,hasWon);
-			result = Picture();
+			result = Picture(title,dashes,incorrect,word,drawing,incorrectGuesses,hasWon);
 			System.out.println(result);
 		}
 	}
@@ -30,13 +32,21 @@ public class HangmanRunner
 		incorrectGuesses = man.getAmtIncorrectGuesses();
 		hasWon = man.win();
 	}
-	public static String Picture()
+	public static String Picture(String title, String dashes, String incorrect,String word,String drawing,int incorrectGuesses,boolean hasWon)
 	{
+		HangMan man = new HangMan();
+		String guess = "";
 		Scanner sc = new Scanner(System.in);
 		String result = "";
 		result += title + "\nWord: " + dashes + "\n" + drawing + "\nIncorrect Guesses: " + incorrect;
-		result += "\tNumber Incorrect: " + incorrectGuesses + "\nYour Guess: " + word;
-		word = sc.nextLine();
+		result += "\tNumber Incorrect: " + incorrectGuesses + "\nYour Guess: ";
+		guess = sc.nextLine();
+		if(guess.length() > 1)
+			man.guessWord(guess);
+		else
+			man.guessLetter(guess.charAt(0));
+
+		result += guess;
 
 		return result;
 
